2337d7a7b7b358852abaa9220833f58033a231fc
Moni0305 <148535954+Moni0305@users.noreply.github.com>
2023-10-20 14:36:03 +0530

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,14 @@
+class student:
+def _int__(self,name,roll,_number,cgpa):
+self.name=name
+self.roll_number=roll_number
+self.cgpa=cgpa
+def sort_student=(student_list):
+sorted_student=Dorted(student_list,key=lambda student: student s,cgpa,reverse=Tree)
+return sorted_students
+students=[student("Hari","A123,7.8"),                 student("Srikanth","A124",8.9),student("Saumya","A7125",9.1), student("Mahidhar","A126",9.9)]
+sorted_student=sort_student(students)
+for students in sorted_students
+ print("name:{},roll_number:{},CGPA:{}".format(student,cgpa))
+  
+                  
